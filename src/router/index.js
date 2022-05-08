fed66992e903a1fa4c418663d76990a112146a25
SinxLinonZ <urara.kbys@gmail.com>
2022-05-09 08:38:16 +0900

    Add: Route title

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,29 @@ import RTLecture from "@/views/RTLecture.vue";
 import LectureCreator from "@/views/LectureCreator.vue";
 
 const routes = [
-  { path: "/", component: RTLecture },
-  { path: "/creator", component: LectureCreator },
+  {
+    path: "/",
+    component: RTLecture,
+    name: "rt-lecture",
+    meta: { title: "RTLecture" },
+  },
+  {
+    path: "/creator",
+    component: LectureCreator,
+    name: "LectureCreator",
+    meta: { title: "Lecture Creator" },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Set route title
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
